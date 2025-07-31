2ae78ce0fd8981f10da41a07ddfc4519f0d5420d
prabha <prabhamtechsrm@gmail.com>
2025-07-31 18:16:27 +0530

    additional test cases added

--- tshirts.py
+++ tshirts.py
@@ -11,4 +11,12 @@ def size(cms):
 assert(size(37) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
+assert(size(37) == 'S')
+assert(size(38) == 'S')
+assert(size(36) == 'M')
+assert(size(38) == 'M')
+assert(size(42) == 'M')
+assert(size(40) == 'L')
+assert(size(42) == 'L')
+
 print("All is well (maybe!)")
